7aa0cf3c7c797889e7f6eadb2284b8b8cde06aaf
puringlascoffee <espinozagerardo171@gmail.com>
2025-08-31 13:29:56 -0600

    initial commit

---
+++ comandos.js
@@ -0,0 +1,50 @@
+const navLinks=document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton=document.querySelector("#menu-open-button");
+const menuCloseButton=document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click",() =>{
+document.body.classList.toggle("show-mobile-menu")
+
+});
+
+menuCloseButton.addEventListener("click",() => menuOpenButton.click());
+//CIERRA EL MENU CUANDO EL SE TOCA UN BOTON(CELULARES)
+navLinks.forEach(link=>{
+link.addEventListener("click",() => menuOpenButton.click());
+});
+//INICIA EL SLIDE
+const swiper = new Swiper('.slider-wrapper',{
+  loop: true,
+  spaceBetween:25,
+  grabCursor:true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+    0:{
+        slidesPerView:1
+    },
+
+    768:{
+        slidesPerView:2
+    },
+
+1024:{
+        slidesPerView:3
+    },
+
+
+  }
+  },
+);
